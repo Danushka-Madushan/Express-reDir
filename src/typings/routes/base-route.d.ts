ab89813842b95a4a191cc07a602011bd046b0c64
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-15 22:57:14 +0530

    typings

--- src/typings/routes/base-route.d.ts
+++ src/typings/routes/base-route.d.ts
@@ -5,19 +5,8 @@ declare module 'routes/base-route' {
     }
 
     export namespace queryRequest {
-        interface TParams {
-            id: string,
-            city: string
-        }
-
-        interface TReq {
-            id: string,
-            data: Data
-        }
-
         interface TQuery {
-            year?: string,
-            user?: string
+            url?: string
         }
     }
 
