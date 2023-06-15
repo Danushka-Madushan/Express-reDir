39755770be64b9ec8db7572e1cdad691963743e1
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-15 19:43:40 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,3 @@
 import { config } from 'dotenv';config();
-import { TPerson } from 'config';
 
 export const PORT = 8080
-
-export const SampleFunction = (): TPerson => {
-    return {
-        name: "Alex Murphy",
-        address: [
-            {
-                key: "District",
-                value: "Hambantota",
-                country: "LK"
-            }
-        ],
-        age: 12
-    } satisfies TPerson
-}
