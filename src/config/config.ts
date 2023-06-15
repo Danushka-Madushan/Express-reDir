a587831c9ba7b80639c2358f01d55cbefaeb2745
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-15 20:46:15 +0530

    Environment configs moved to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,7 @@
-// import { config } from 'dotenv';config();
-
 export const PORT = 8080
+
+export const ENV = {
+    NODE_ENV: process.env.NODE_ENV || 'production',
+    PORT: process.env.PORT,
+    NPM_VERSION: process.env.npm_package_version
+}
