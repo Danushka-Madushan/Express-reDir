a3a291e0e2128967ccfadc0d13eb97ae97f4cf97
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-16 23:07:44 +0530

    update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,14 @@
 export const PORT = 8080
 
 export const ENV = {
+    /* Node Environment */
     NODE_ENV: process.env.NODE_ENV || 'production',
+    /* Express app Port */
     PORT: process.env.PORT,
+    /* Npm Package Version */
     NPM_VERSION: process.env.npm_package_version,
+    /* Redis API Key */
     REDIS_KEY: process.env.REDIS_KEY,
+    /* Redis API */
     REDIS_API: process.env.REDIS_API
 }
