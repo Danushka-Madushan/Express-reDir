4e3f1b6782ce3e31fce0b1147905de8991cc8c2b
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-20 02:02:08 +0530

    Update dev.ts

--- src/core/utils/dev.ts
+++ src/core/utils/dev.ts
@@ -1,5 +1,6 @@
 import { ENV } from '../../config/config.js'
 
+/* Development Log (Only works Under development ENV) */
 export const DevelopmentLog = (log: string): void => {
     if (ENV.NODE_ENV === 'development') {
         console.log(log)
