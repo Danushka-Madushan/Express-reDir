007941998cf16028801f6feb5f0f74eb03b94407
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-06-20 02:02:11 +0530

    Update keyGen.ts

--- src/core/utils/keyGen.ts
+++ src/core/utils/keyGen.ts
@@ -1,5 +1,9 @@
 import { insertRecord } from '../redis/cache.js'
 
+/*
+    Generate a random string of given length after generating it will check if there is
+    aleready key containing under that key if present it will generate a new key
+*/
 export const generateKey = async (length: number, url: string): Promise<string> => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
     const charactersLength = characters.length
